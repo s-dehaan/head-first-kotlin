e767d0b56a8d79b9a46b368f3451dd328ae9f20c
Sebastiaan de Haan <79367966+s-dehaan@users.noreply.github.com>
2023-04-23 19:28:21 +0200

    Added my first loop

--- chapter_1/src/main/kotlin/Main.kt
+++ chapter_1/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    println("Pow!")
+    var x = 1
+    println("Before the loop. x = $x.")
+    while (x < 4) {
+        println("In the loop. x = $x")
+        x += 1
+    }
+    println("After the loop. x = $x")
 }
