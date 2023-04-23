ab530b13f586c6aaf7d61bb08d22fbc1cca251c9
Sebastiaan de Haan <79367966+s-dehaan@users.noreply.github.com>
2023-04-23 19:34:21 +0200

    concise if statements

--- chapter_1/src/main/kotlin/Main.kt
+++ chapter_1/src/main/kotlin/Main.kt
@@ -1,9 +1,6 @@
 fun main() {
-    var x = 1
-    println("Before the loop. x = $x.")
-    while (x < 4) {
-        println("In the loop. x = $x")
-        x += 1
-    }
-    println("After the loop. x = $x")
+    val x = 3
+    val y = 1
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")
 }
