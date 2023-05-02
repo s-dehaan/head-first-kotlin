bb1f99fe87476d11528884c8f2e8c46dd8d2ab2e
Sebastiaan de Haan <79367966+s-dehaan@users.noreply.github.com>
2023-05-02 18:23:21 +0200

    Chapter 2

--- chapter_1/src/main/kotlin/Main.kt
+++ chapter_1/src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
 fun main() {
-    val x = 3
-    val y = 1
-    println(if (x > y) "x is greater than y" else "x is not greater than y")
-    println("This line runs no matter what")
+    var x = 0
+    var y = 0
+    while (x < 5) {
+        if (y < 5) {
+            x = x + 1
+            if (y < 3) x = x - 1
+        }
+        y = y + 3
+
+        print("$x$x ")
+        x += 1
+    }
 }
